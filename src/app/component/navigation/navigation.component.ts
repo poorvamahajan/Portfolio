5fb3fe08c34a98e8e1e3d6a46517fac379494ee6
poorvamahajan <123051946+poorvamahajan@users.noreply.github.com>
2025-04-09 22:20:41 +0530

    Navigation Bar Made Responsive

--- src/app/component/navigation/navigation.component.ts
+++ src/app/component/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import{RouterLink, RouterLinkActive} from '@angular/router'
 
@@ -8,6 +8,26 @@ import{RouterLink, RouterLinkActive} from '@angular/router'
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css'
 })
-export class NavigationComponent {
+export class NavigationComponent implements AfterViewInit  {
+ngAfterViewInit(): void {
+    const hamburger = document.getElementById('hamburger');
+    const navList = document.getElementById('navList');
+
+    hamburger?.addEventListener('click', () => {
+      navList?.classList.toggle('show');
+      console.log("CLicked");
+    });
+  }
+// @ViewChild('hamburger') hamburgerRef!: ElementRef;
+// @ViewChild('navList') navListRef!: ElementRef;
+
+// ngAfterViewInit(): void {
+//   const hamburger = this.hamburgerRef.nativeElement;
+//   const navList = this.navListRef.nativeElement;
+
+//   hamburger.addEventListener('click', () => {
+//     navList.classList.toggle('show');
+//   });
+// }
 
 }
