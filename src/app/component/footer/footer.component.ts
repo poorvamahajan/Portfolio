d282200c735a905cf217edc9b154835d3311fc1f
poorvamahajan <123051946+poorvamahajan@users.noreply.github.com>
2026-02-02 21:02:19 +0530

    Deployment changes

--- src/app/component/footer/footer.component.ts
+++ src/app/component/footer/footer.component.ts
@@ -9,10 +9,10 @@ import { Component } from '@angular/core';
 export class FooterComponent {
   footer = {
     socialMediaHandle: [
-      { ImageLocation: "../assets/images/icons/icons8-instagram.svg", url: "https://www.instagram.com/__.poorva.__/" },
-      { ImageLocation: "../assets/images/icons/icons8-linkedin-480.svg", url: "https://www.linkedin.com/in/poorva-mahajan-66455b22b/" },
-      { ImageLocation: "../assets/images/icons/icons8-github-288.svg", url: "https://github.com/poorvamahajan" },
-      { ImageLocation: "../assets/images/icons/icons8-leetcode-96.png", url: "https://leetcode.com/u/poorvam14/" },
+      { ImageLocation: "assets/images/icons/icons8-instagram.svg", url: "https://www.instagram.com/__.poorva.__/" },
+      { ImageLocation: "assets/images/icons/icons8-linkedin-480.svg", url: "https://www.linkedin.com/in/poorva-mahajan-66455b22b/" },
+      { ImageLocation: "assets/images/icons/icons8-github-288.svg", url: "https://github.com/poorvamahajan" },
+      { ImageLocation: "assets/images/icons/icons8-leetcode-96.png", url: "https://leetcode.com/u/poorvam14/" },
     ]
   }
 }
